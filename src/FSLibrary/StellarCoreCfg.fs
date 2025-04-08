b76cb3c575c91e04d26642771e3d4783728f4775
marta-lokhova <marta@stellar.org>
2025-04-08 15:40:18 -0700

    Add new configs for testing

--- src/FSLibrary/StellarCoreCfg.fs
+++ src/FSLibrary/StellarCoreCfg.fs
@@ -262,6 +262,8 @@ type StellarCoreCfg =
         t.Add("ARTIFICIALLY_ACCELERATE_TIME_FOR_TESTING", self.accelerateTime) |> ignore
         t.Add("ARTIFICIALLY_GENERATE_LOAD_FOR_TESTING", self.generateLoad) |> ignore
         t.Add("FLOOD_DEMAND_PERIOD_MS", 100) |> ignore
+        t.Add("SKIP_SCP_PERSISTENCE_FOR_TESTING", true) |> ignore
+        t.Add("TRIGGER_OFFSET_FOR_TESTING", 3) |> ignore
 
         if self.updateSorobanCosts.IsSome then
             t.Add("UPDATE_SOROBAN_COSTS_DURING_PROTOCOL_UPGRADE_FOR_TESTING", self.updateSorobanCosts.Value)
